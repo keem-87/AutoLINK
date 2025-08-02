2774295f229a477550723ccd7fe6e4ecdccefda3
Rackeem Reid <78712819+keem-87@users.noreply.github.com>
2025-08-01 19:43:21 -0700

    Update AppRouter.js

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
 import Homejsx from './components/Homejsx';
 import RegisterPagejs from './RegisterPagejs';
 import LoginPagejs from './LoginPagejs';
+import BookingPagejs from './BookingPagejs';
+import RatesPagejs from './RatesPagejs';
 
 function AppRouter() {
   // Use React.createElement instead of JSX to avoid angle bracket issues in this file.
@@ -15,10 +16,11 @@ function AppRouter() {
       null,
       React.createElement(Route, { path: '/', element: React.createElement(Homejsx) }),
       React.createElement(Route, { path: '/register', element: React.createElement(RegisterPagejs) }),
-      React.createElement(Route, { path: '/login', element: React.createElement(LoginPagejs) })
+      React.createElement(Route, { path: '/login', element: React.createElement(LoginPagejs) }),
+      React.createElement(Route, { path: '/booking', element: React.createElement(BookingPagejs) }),
+      React.createElement(Route, { path: '/rates', element: React.createElement(RatesPagejs) })
     )
   );
 }
 
 export default AppRouter;
-
