1c637715bf3020d631bcf8429f18e8ff8569387a
Rackeem Reid <78712819+keem-87@users.noreply.github.com>
2025-08-01 18:33:59 -0700

    Update AppRouter.js

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,31 +1,24 @@
-// AppRouter.js
-// This component defines client-side routing for the Shelter-Stead React app.
-// It uses react-router-dom to map paths to page components. Ensure
-// component filenames match import names (e.g. Homejsx, Registerjs, Login). If you rename
-// components (e.g. Register.js), update imports accordingly.
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-// Import page components. Note: "Registerjs" reflects the file name created earlier.
 import Homejsx from './components/Homejsx';
-import Registerjs from './components/Registerjs';
-import Login from './components/Login';
+import RegisterPagejs from './RegisterPagejs';
+import LoginPagejs from './LoginPagejs';
 
-// The AppRouter component wraps the app in a Router and defines routes.
 function AppRouter() {
-  return (
-    <Router>
-      <Routes>
-        {/* Root path shows the homepage */}
-        <Route path="/" element={<Homejsx />} />
-        {/* Registration form */}
-        <Route path="/register" element={<Registerjs />} />
-        {/* Login form with optional 2FA */}
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+  // Use React.createElement instead of JSX to avoid angle bracket issues in this file.
+  return React.createElement(
+    Router,
+    null,
+    React.createElement(
+      Routes,
+      null,
+      React.createElement(Route, { path: '/', element: React.createElement(Homejsx) }),
+      React.createElement(Route, { path: '/register', element: React.createElement(RegisterPagejs) }),
+      React.createElement(Route, { path: '/login', element: React.createElement(LoginPagejs) })
+    )
   );
 }
 
 export default AppRouter;
+
