05e50ec7146296f91e29a3d7f8fa09fd259b265f
Rackeem Reid <78712819+keem-87@users.noreply.github.com>
2025-08-11 14:04:54 -0700

    Update Navbar.js

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,8 +1,7 @@
 // Navbarjs
 // This component provides navigation links for the application.
-// It uses react-router-dom's Link component to switch between pages without
-// reloading the browser. Additional links can be added as you build more pages.
-
+// It uses react-router-dom's Link component to switch between pages without reloading the browser.
+// Additional links can be added as you build more pages.
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -11,9 +10,17 @@ function Navbar() {
     <nav className="navbar">
       <ul>
         <li><Link to="/">Home</Link></li>
+        <li><Link to="/services">Services</Link></li>
+        <li><Link to="/pricing">Pricing</Link></li>
+        <li><Link to="/faq">FAQ</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/contact">Contact</Link></li>
         <li><Link to="/register">Register</Link></li>
-                <li><Link to="/booking">Booking</Link></li>
+        <li><Link to="/login">Login</Link></li>
+        <li><Link to="/booking">Booking</Link></li>
         <li><Link to="/rates">Rates</Link></li>
+      </ul>
     </nav>
   );
 }
