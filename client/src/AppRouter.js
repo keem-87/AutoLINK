f6dc325f7862b244a2058c12c3a2c9822bbc79ab
Rackeem Reid <78712819+keem-87@users.noreply.github.com>
2025-08-11 18:35:51 -0700

    Update AppRouter.js

--- client/src/AppRouter.js
+++ client/src/AppRouter.js
@@ -1,24 +1,35 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Homejsx from './components/Homejsx';
+import Home from './pages/Home';
 import RegisterPagejs from './RegisterPagejs';
 import LoginPagejs from './LoginPagejs';
 import BookingPagejs from './BookingPagejs';
 import RatesPagejs from './RatesPagejs';
+import Services from './pages/Services';
+import Pricing from './pages/Pricing';
+import Faq from './pages/Faq';
+import Contact from './pages/Contact';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
 
 function AppRouter() {
-  // Use React.createElement instead of JSX to avoid angle bracket issues in this file.
   return React.createElement(
     Router,
     null,
     React.createElement(
       Routes,
       null,
-      React.createElement(Route, { path: '/', element: React.createElement(Homejsx) }),
+      React.createElement(Route, { path: '/', element: React.createElement(Home) }),
       React.createElement(Route, { path: '/register', element: React.createElement(RegisterPagejs) }),
       React.createElement(Route, { path: '/login', element: React.createElement(LoginPagejs) }),
       React.createElement(Route, { path: '/booking', element: React.createElement(BookingPagejs) }),
-      React.createElement(Route, { path: '/rates', element: React.createElement(RatesPagejs) })
+      React.createElement(Route, { path: '/rates', element: React.createElement(RatesPagejs) }),
+      React.createElement(Route, { path: '/services', element: React.Services(Services) }),
+      React.createElement(Route, { path: '/pricing', element: React.createElement(Pricing) }),
+      React.createElement(Route, { path: '/faq', element: React.createElement(Faq) }),
+      React.createElement(Route, { path: '/contact', element: React.createElement(Contact) }),
+      React.createElement(Route, { path: '/about', element: React.createElement(About) }),
+      React.createElement(Route, { path: '/gallery', element: React.createElement(Gallery) })
     )
   );
 }
